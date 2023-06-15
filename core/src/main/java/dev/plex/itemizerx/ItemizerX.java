dc1b460c6a61cbbc7d37d9b0838b65b3c65c68b7
Telesphoreo <me@telesphoreo.me>
2023-06-15 17:57:36 -0500

    FIx initial errors

--- core/src/main/java/dev/plex/itemizerx/ItemizerX.java
+++ core/src/main/java/dev/plex/itemizerx/ItemizerX.java
@@ -20,35 +20,43 @@ public class ItemizerX extends JavaPlugin {
             case "v1_20_R1": {
                 getCommand("itemizer").setExecutor(new dev.plex.itemizerx.v1_20_R1.ItemizerXCommand());
                 getCommand("itemizer").setTabCompleter(new dev.plex.itemizerx.v1_20_R1.ItemizerXTab());
+                return;
             }
             case "v1_19_R3": {
                 getCommand("itemizer").setExecutor(new dev.plex.itemizerx.v1_19_R3.ItemizerXCommand());
                 getCommand("itemizer").setTabCompleter(new dev.plex.itemizerx.v1_19_R3.ItemizerXTab());
+                return;
             }
             case "v1_19_R2": {
                 getCommand("itemizer").setExecutor(new dev.plex.itemizerx.v1_19_R2.ItemizerXCommand());
                 getCommand("itemizer").setTabCompleter(new dev.plex.itemizerx.v1_19_R2.ItemizerXTab());
+                return;
             }
             case "v1_19_R1": {
                 getCommand("itemizer").setExecutor(new dev.plex.itemizerx.v1_19_R1.ItemizerXCommand());
                 getCommand("itemizer").setTabCompleter(new dev.plex.itemizerx.v1_19_R1.ItemizerXTab());
+                return;
             }
             case "v1_18_R2": {
                 getCommand("itemizer").setExecutor(new dev.plex.itemizerx.v1_18_R2.ItemizerXCommand());
                 getCommand("itemizer").setTabCompleter(new dev.plex.itemizerx.v1_18_R2.ItemizerXTab());
+                return;
             }
             case "v1_18_R1": {
                 getCommand("itemizer").setExecutor(new dev.plex.itemizerx.v1_18_R1.ItemizerXCommand());
                 getCommand("itemizer").setTabCompleter(new dev.plex.itemizerx.v1_18_R1.ItemizerXTab());
+                return;
             }
             case "v1_17_R1": {
                 getCommand("itemizer").setExecutor(new dev.plex.itemizerx.v1_17_R1.ItemizerXCommand());
                 getCommand("itemizer").setTabCompleter(new dev.plex.itemizerx.v1_17_R1.ItemizerXTab());
+                return;
             }
-            default:
+            default: {
                 getLogger().severe("You are trying to run ItemizerX on an incompatible server version.");
                 getLogger().severe("ItemizerX only supports versions 1.17 to 1.20, disabling plugin.");
                 getServer().getPluginManager().disablePlugin(this);
+            }
         }
     }
 
